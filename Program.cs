0f7a72a3bbbc3ec5edd37d261a6c264d6a8c9507
Gonoboben <126049477+Gonoboben@users.noreply.github.com>
2026-06-26 09:02:36 +0300

    Add Program entry point

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+using Avalonia;
+
+namespace BuoyCalc.Windows;
+
+internal static class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
+
+    public static AppBuilder BuildAvaloniaApp()
+    {
+        return AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+    }
+}
