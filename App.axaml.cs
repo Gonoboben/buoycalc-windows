79b6bfa7bb4c8c8e0594839345446a54b9bf81fb
Gonoboben <126049477+Gonoboben@users.noreply.github.com>
2026-06-26 09:09:28 +0300

    Implement Avalonia app startup

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,5 +1,24 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using BuoyCalc.Windows.Views;
+
 namespace BuoyCalc.Windows;
 
-public partial class App
+public partial class App : Application
 {
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow();
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
 }
