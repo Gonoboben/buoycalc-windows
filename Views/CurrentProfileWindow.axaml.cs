563fa8925a38bacb4ef161617633e09171a69395
Gonoboben <126049477+Gonoboben@users.noreply.github.com>
2026-06-27 10:43:54 +0300

    Add current profile editor code-behind

---
+++ Views/CurrentProfileWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BuoyCalc.Windows.Views;
+
+public partial class CurrentProfileWindow : Window
+{
+    public CurrentProfileWindow()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
