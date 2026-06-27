57b9c77360b90e8972ae1bf60242ba5ad931bf06
Gonoboben <126049477+Gonoboben@users.noreply.github.com>
2026-06-27 21:07:23 +0300

    Add full report text window code-behind

---
+++ Views/ReportTextWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BuoyCalc.Windows.Views;
+
+public partial class ReportTextWindow : Window
+{
+    public ReportTextWindow()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
