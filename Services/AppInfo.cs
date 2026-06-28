80fe057e75f70dfc42a507b12647b014e6e44858
Gonoboben <126049477+Gonoboben@users.noreply.github.com>
2026-06-28 15:05:31 +0300

    Bump application version to v0.30

--- Services/AppInfo.cs
+++ Services/AppInfo.cs
@@ -2,9 +2,9 @@ namespace BuoyCalc.Windows.Services;
 
 public static class AppInfo
 {
-    public const string Version = "v0.29";
-    public const string VersionNote = "iterative shape solver";
+    public const string Version = "v0.30";
+    public const string VersionNote = "shape projection bridge";
 
-    public static string WindowTitle => $"BuoyCalc Windows {Version}";
-    public static string DisplayVersion => $"{Version} · {VersionNote}";
+    public static string WindowTitle => "BuoyCalc Windows " + Version;
+    public static string DisplayVersion => Version + " · " + VersionNote;
 }
