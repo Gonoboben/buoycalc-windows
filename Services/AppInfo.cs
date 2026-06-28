c2d76ee2f7d19a56d8874a282cb8d032ab376ffc
Gonoboben <126049477+Gonoboben@users.noreply.github.com>
2026-06-28 17:49:45 +0300

    Bump AppInfo to v0.34

--- Services/AppInfo.cs
+++ Services/AppInfo.cs
@@ -2,8 +2,8 @@ namespace BuoyCalc.Windows.Services;
 
 public static class AppInfo
 {
-    public const string Version = "v0.33";
-    public const string VersionNote = "sequence positions";
+    public const string Version = "v0.34";
+    public const string VersionNote = "tension report";
 
     public static string WindowTitle => "BuoyCalc Windows " + Version;
     public static string DisplayVersion => Version + " - " + VersionNote;
