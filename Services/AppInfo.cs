11fc597b476ed1b5962b86ea70f91ab697ec5cb1
Gonoboben <126049477+Gonoboben@users.noreply.github.com>
2026-06-29 19:03:24 +0300

    Align AppInfo with v0.39.1 convergence criteria

--- Services/AppInfo.cs
+++ Services/AppInfo.cs
@@ -3,7 +3,7 @@ namespace BuoyCalc.Windows.Services;
 public static class AppInfo
 {
     public const string Version = "v0.39.1";
-    public const string VersionNote = "iterative shape comparison";
+    public const string VersionNote = "solver convergence criteria";
 
     public static string WindowTitle => "BuoyCalc Windows " + Version;
     public static string DisplayVersion => Version + " - " + VersionNote;
