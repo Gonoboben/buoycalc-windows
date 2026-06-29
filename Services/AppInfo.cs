228888a71f2f7e11c3192148c401eea594e28c26
Gonoboben <126049477+Gonoboben@users.noreply.github.com>
2026-06-29 19:29:37 +0300

    Bump AppInfo to v0.40.2

--- Services/AppInfo.cs
+++ Services/AppInfo.cs
@@ -2,8 +2,8 @@ namespace BuoyCalc.Windows.Services;
 
 public static class AppInfo
 {
-    public const string Version = "v0.40.1";
-    public const string VersionNote = "primary shape selection report";
+    public const string Version = "v0.40.2";
+    public const string VersionNote = "primary shape selection table";
 
     public static string WindowTitle => "BuoyCalc Windows " + Version;
     public static string DisplayVersion => Version + " - " + VersionNote;
