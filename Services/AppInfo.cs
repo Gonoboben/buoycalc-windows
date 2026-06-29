28af5e063da382a7dc66a98402b1920c9ca150c1
Gonoboben <126049477+Gonoboben@users.noreply.github.com>
2026-06-29 19:37:14 +0300

    Bump AppInfo to v0.42

--- Services/AppInfo.cs
+++ Services/AppInfo.cs
@@ -2,8 +2,8 @@ namespace BuoyCalc.Windows.Services;
 
 public static class AppInfo
 {
-    public const string Version = "v0.41";
-    public const string VersionNote = "deployment modes";
+    public const string Version = "v0.42";
+    public const string VersionNote = "scenario autochecks";
 
     public static string WindowTitle => "BuoyCalc Windows " + Version;
     public static string DisplayVersion => Version + " - " + VersionNote;
