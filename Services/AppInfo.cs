96bf12e6468b0a4bea3933c4a88029ab98366406
Gonoboben <126049477+Gonoboben@users.noreply.github.com>
2026-06-29 20:54:55 +0300

    Bump AppInfo to v0.46.2

--- Services/AppInfo.cs
+++ Services/AppInfo.cs
@@ -2,8 +2,8 @@ namespace BuoyCalc.Windows.Services;
 
 public static class AppInfo
 {
-    public const string Version = "v0.46.1";
-    public const string VersionNote = "PDF report cleanup";
+    public const string Version = "v0.46.2";
+    public const string VersionNote = "user and full reports";
 
     public static string WindowTitle => "BuoyCalc Windows " + Version;
     public static string DisplayVersion => Version + " - " + VersionNote;
