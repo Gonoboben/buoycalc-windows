8e386cf3745aee29d367abcbe46e14e8af0f6f1f
Gonoboben <126049477+Gonoboben@users.noreply.github.com>
2026-08-13 21:15:24 +0300

    Update validation entry point

--- validation/BuoyCalc.EngineeringRegression/ValidationEntryPoint.cs
+++ validation/BuoyCalc.EngineeringRegression/ValidationEntryPoint.cs
@@ -14,6 +14,7 @@ internal static class ValidationEntryPoint
             BerteauxConstitutiveDragBoundaryRegression.Validate();
             BerteauxPlanarResistanceVectorRegression.Validate();
             UniformCurrentReadModelRegression.Validate();
+            UniformCurrentReportRegression.Validate();
             SignedNodeEquilibriumRegression.Validate();
             FinalIterationDiscreteStateRegression.Validate();
             FinalIterationSignedNodeEquilibriumRegression.Validate();
