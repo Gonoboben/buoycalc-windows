77b228ed3982d7305ef697efa243a05bb5b62199
Gonoboben <126049477+Gonoboben@users.noreply.github.com>
2026-08-13 13:57:47 +0300

    Validation: restore standard regression entrypoint

--- validation/BuoyCalc.EngineeringRegression/ValidationEntryPoint.cs
+++ validation/BuoyCalc.EngineeringRegression/ValidationEntryPoint.cs
@@ -11,7 +11,6 @@ internal static class ValidationEntryPoint
             SignedNodeEquilibriumRegression.Validate();
             FinalIterationDiscreteStateRegression.Validate();
             FinalIterationSignedNodeEquilibriumRegression.Validate();
-            SurfaceBoundaryShootingEvidence.Print();
         }
         catch (Exception ex)
         {
